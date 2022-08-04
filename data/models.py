5093c1480895469325a34768530efe7bd921a367
Alireza Shirmohammad <alirezashirmohammad55@gmail.com>
2022-08-04 14:13:25 +0430

    Add: Models to data.models

--- data/models.py
+++ data/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+User = ''
+
+class Lesson(models.Model):
+    name = models.CharField
+
+    class Meta:
+        verbose_name = 'درس'
+        verbose_name_plural = 'دروس'
+
+
+class Class(models.Model):
+    students = models.ForeignKey
+    teacher = models.ManyToManyField
+
+    class Meta:
+        verbose_name = 'کلاس'
+        verbose_name_plural = 'کلاس ها'
+
+
+class Grade(models.Model):
+    classes = models.ForeignKey(Class, verbose_name='کلاس ها')
+    lessons = models.ForeignKey(Lesson, verbose_name='دروس')
+    students = models.ForeignKey(User, on_delete=models.DO_NOTHING())  # can i have a queryset here? for example class.students
+
+
+class ReportCard(models.Model):
+    student = models.ForeignKey(User, models.DO_NOTHING())
+    lessons = models.ManyToManyField(Lesson, on_delete=models.DO_NOTHING())
