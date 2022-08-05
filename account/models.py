c0574e8e8133392bca70c73d732cce83a54132c3
Alireza Shirmohammad <alirezashirmohammad55@gmail.com>
2022-08-05 17:57:36 +0430

    Add: User base model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
+
+
+class User(AbstractBaseUser):
+    name = models.CharField
+    last_name = models.CharField
+    father_name = models.CharField
+    national_code = models.CharField
+    hamgam_code = models.CharField
+    avatar = models.ImageField
+    is_student = models.BooleanField
+    is_teacher = models.BooleanField
+
