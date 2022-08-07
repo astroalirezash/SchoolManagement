4c296c8f2555eb198e591a40aea284ac4c117388
Alireza Shirmohammad <alirezashirmohammad55@gmail.com>
2022-08-07 22:51:21 +0430

    Fix: account.models.User

--- account/models.py
+++ account/models.py
@@ -5,12 +5,33 @@ from django.contrib.auth.models import AbstractBaseUser
 
 
 class User(AbstractBaseUser):
-    name = models.CharField
-    last_name = models.CharField
-    father_name = models.CharField
-    national_code = models.CharField
-    hamgam_code = models.CharField
-    avatar = models.ImageField
-    is_student = models.BooleanField
-    is_teacher = models.BooleanField
+    username = models.CharField(
+        max_length=40, unique=True
+        )
+    name = models.CharField(
+        max_length=30, verbose_name='نام'
+        )
+    last_name = models.CharField(
+        max_length=60, verbose_name='نام خانوادگی'
+        )
+    father_name = models.CharField(
+        max_length=30, verbose_name='نام پدر', null=False, blank=False
+        )
+    national_code = models.CharField(
+        max_length=10, verbose_name='کد ملی', null=False, blank='False'
+        )
+    hamgam_or_pressonel_code = models.CharField(
+        max_length=25, verbose_name='کد همگام/پرسنلی'
+        )
+    avatar = models.ImageField(
+        null=False, blank=False, upload_to='account/avatar/', default='defualts/user.png'
+    )
+    is_student = models.BooleanField(
+        verbose_name='دانش آموز', default=True
+    )
+    is_teacher = models.BooleanField(
+        verbose_name='معلم', default=False
+    )
+
+    USERNAME_FIELD = 'username'
 
--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # My Apps:
+    'account'
 ]
 
 MIDDLEWARE = [
@@ -115,9 +119,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'assets'
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'account.User'
