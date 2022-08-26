6b63e7d8af2e3c14b632a37c7f97d80a5a16c1af
Alireza Shirmohammad <alirezashirmohammad55@gmail.com>
2022-08-26 15:52:56 +0430

    Add: Login page

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
+from .views import (
+    SignInView
+)
 
 app_name = 'account'
 
 urlpatterns = [
-
+    path('login', SignInView.as_view(), name='login')
 ]
--- account/views.py
+++ account/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+
+# from .forms import (
+#     LoginForm
+# )
 
 # Create your views here.
+
+
+class SignInView(LoginView):
+    # form_class = LoginForm
+    template_name = 'account/login.html'
