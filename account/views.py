179fdbec7debabcd95bf8da2a6d3cfc066ae373c
Alireza Shirmohammad <alirezashirmohammad55@gmail.com>
2022-08-28 00:04:09 +0430

    Update: templates/data/ReportCard.html

--- account/views.py
+++ account/views.py
@@ -1,13 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView
 
-# from .forms import (
-#     LoginForm
-# )
-
 # Create your views here.
 
 
 class SignInView(LoginView):
-    # form_class = LoginForm
     template_name = 'account/login.html'
