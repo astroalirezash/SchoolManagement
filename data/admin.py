fdbf018d352ccf10ee4d44f5bee149804b1413d8
Alireza Shirmohammad <alirezashirmohammad55@gmail.com>
2022-08-29 22:36:42 +0430

    Add: Registering Models

--- data/admin.py
+++ data/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import (
+    Class,
+    Grade,
+    ReportCard,
+    Lesson
+)
+
+
 # Register your models here.
+
+admin.site.register(Class)
+admin.site.register(Grade)
+admin.site.register(ReportCard)
+admin.site.register(Lesson)
