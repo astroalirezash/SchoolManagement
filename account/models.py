0dae2b13a8e7be2e7afd34e7c9f33977bd6bd6ab
Alireza Shirmohammad <alirezashirmohammad55@gmail.com>
2022-08-29 23:36:13 +0430

    date_joined, full_name() and __str__() to account.User

--- account/models.py
+++ account/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.utils import timezone
 
 # Create your models here.
 
@@ -52,35 +53,52 @@ class UserManager(BaseUserManager):
 class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(
         max_length=30, verbose_name='نام'
-        )
+    )
     last_name = models.CharField(
         max_length=60, verbose_name='نام خانوادگی'
-        )
+    )
     father_name = models.CharField(
         max_length=30, verbose_name='نام پدر', null=False, blank=False
-        )
+    )
     national_code = models.CharField(
         max_length=10, verbose_name='کد ملی', null=False, blank=False, unique=True
-        )
+    )
     hamgam_or_pressonel_code = models.CharField(
         max_length=25, verbose_name='کد همگام/پرسنلی'
-        )
+    )
     avatar = models.ImageField(
-        null=False, blank=False, upload_to='account/avatar/', default='defualts/user.png'
-        )
+        null=False, blank=False, upload_to='account/avatar/', default='defualts/user.png', verbose_name='عکس'
+    )
+    date_joined = models.DateTimeField(
+        verbose_name='تاریخ اضافه شدن', default=timezone.now
+    )
     is_student = models.BooleanField(
         verbose_name='دانش آموز', default=True
-        )
+    )
     is_teacher = models.BooleanField(
         verbose_name='معلم', default=False
-        )
+    )
     is_active = models.BooleanField(
         default=True, verbose_name='فعال / غیر فعال'
-        )
+    )
     is_staff = models.BooleanField(
         verbose_name='فعال / غیرفعال', default=False
-        )
+    )
 
     USERNAME_FIELD = 'national_code'
 
     objects = UserManager()
+
+    class Meta:
+        verbose_name = 'کاربر'
+        verbose_name_plural = 'کاربران'
+
+    def full_name(self):
+        if self.name or self.last_name:
+            f_name = f'{self.name} {self.last_name}'
+        else:
+            f_name = 'بدون نام'
+        return f_name
+    
+    def __str__(self):
+        return f'{self.full_name()}'
