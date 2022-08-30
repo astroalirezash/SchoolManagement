8a173351900fa05218acffd59c8998f085ca0436
Alireza Shirmohammad <alirezashirmohammad55@gmail.com>
2022-08-31 00:01:48 +0430

    Add: ReportCard view

--- data/urls.py
+++ data/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
+from .views import (
+    RcardView
+)
+
 
 app_name = 'data'
 
 urlpatterns = [
-
+    path('report-card', RcardView.as_view(), name='rcard')
 ]
--- data/views.py
+++ data/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http.response import Http404
+
+from .models import (
+    ReportCard
+)
+from account.models import User
 
 # Create your views here.
 
-def test(request):
-    return render(request, 'data/ReportCard.html')
+
+class RcardView(LoginRequiredMixin, View):
+
+    def get(self, request):
+        user = request.user.id
+        rcard = get_object_or_404(ReportCard, student_id=user)
+        context = {
+            'rcard': rcard
+        }
+        return render(request, 'data/ReportCard.html', context)            
