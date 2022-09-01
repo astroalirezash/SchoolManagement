8ab670a7605fae2e4e95bc2a3bb93ff94f8ca819
Alireza Shirmohammad <alirezashirmohammad55@gmail.com>
2022-09-02 00:01:55 +0430

    Add News model to content.models

--- content/models.py
+++ content/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from ckeditor.fields import RichTextField
+from django.utils import timezone
 
 # Create your models here.
+
+
+class News(models.Model):
+    title = models.CharField(
+        verbose_name='تایل', max_length=50
+    )
+    text = RichTextField(
+        null=True, blank=True
+    )
+    date_added = models.DateTimeField(
+        verbose_name='تاریخ اضافه شدن', default=timezone.now
+    )
+    to_other_pages = models.BooleanField(
+        default=True
+    )
+
+    class Meta:
+        verbose_name = 'خبر'
+        verbose_name_plural = 'اخبار'
+
+    def __str__(self):
+        return f'{self.title} | {self.date_added}'
+    
+
