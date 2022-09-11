b4316a62bc7ffc410f37a0bffcce3953fc3764b0
Alireza Shirmohammad <alirezashirmohammad55@gmail.com>
2022-09-12 00:10:01 +0430

    Add: ReportCardAdmin

--- data/admin.py
+++ data/admin.py
@@ -8,11 +8,19 @@ from .models import (
     Score
 )
 
+from account.models import User
 
 # Register your models here.
 
+
+@admin.register(ReportCard)
+class ReportCardAdmin(admin.ModelAdmin):
+    def render_change_form(self, request, context, *args, **kwargs):
+         context['adminform'].form.fields['student'].queryset = User.objects.filter(is_student=True)
+         return super(ReportCardAdmin, self).render_change_form(request, context, *args, **kwargs)
+
+
 admin.site.register(Class)
 admin.site.register(Grade)
-admin.site.register(ReportCard)
 admin.site.register(Lesson)
 admin.site.register(Score)
